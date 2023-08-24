582b952e94891469c16be574c6d9d6c531420250
PABLO NAVARRO <118768679+pablonq@users.noreply.github.com>
2023-08-24 16:38:37 -0300

    Update procesaEjercicio4.php

--- tpn2/Vista/ejercicio4/procesaEjercicio4.php
+++ tpn2/Vista/ejercicio4/procesaEjercicio4.php
@@ -10,22 +10,22 @@
             -->
 <div class="contenedorCentrado">
 <div class='content rounded-1 shadow'>
-                <div class='header border-bottom-0'>
+                <div class="header border-bottom-0 d-flex align-items-right">
                     <h1>La pelicula introducida es</h1>
-                    <a href='ejercicio_4.php'><button type='button' class='btn-close' data-bs-dismiss='modal' aria-label='Close'></button></a>
+                    <a href='../Vista/ejercicio4.php'><button type='button' class='btn-close' data-bs-dismiss='modal' aria-label='Close'></button></a>
                 </div>
                 <div class='body py-0'>
-                    <span class='texto'>Titulo: </span><span class='descripcion'><?php echo $titulo = $_post["titulo"]; ?></span></br>
-                    <span class='texto'>Actores: </span><span class='descripcion'><?php echo $actores = $_post["actores"]; ?></span></br>
-                    <span class='texto'>Director: </span><span class='descripcion'><?php echo $director = $_post["director"]; ?></span></br>
-                    <span class='texto'>Guion: </span><span class='descripcion'><?php echo $guion = $_post["guion"]; ?></span></br>
-                    <span class='texto'>Produccion: </span><span class='descripcion'><?php echo $produccion = $_post["produccion"]; ?></span></br>
-                    <span class='texto'>Año: </span><span class='descripcion'><?php echo $anio = $_post["anio"]; ?></span></br>
-                    <span class='texto'>Nacionalidad: </span><span class='descripcion'><?php echo $nacionalidad = $_post["nacionalidad"]; ?></span></br>
-                    <span class='texto'>Genero: </span><span class='descripcion'><?php echo $genero = $_post["genero"]; ?></span></br>
-                    <span class='texto'>Duracion: </span><span class='descripcion'><?php echo $duracion = $_post["duracion"]; ?></span></br>
-                    <span class='texto'>Restricciones de Edad: </span><span class='descripcion'><?php echo $restriccion = $_post["edad"]; ?></span></br>
-                    <span class='texto'>Sinopsis: </span><span class='descripcion'><?php echo $sinopsis = $_post["sinopsis"]; ?></span></br>
+                    <span class='texto'>Titulo: </span><span class='descripcion'><?php echo $titulo = $_POST['titulo']; ?></span></br>
+                    <span class='texto'>Actores: </span><span class='descripcion'><?php echo $actores = $_POST['actores']; ?></span></br>
+                    <span class='texto'>Director: </span><span class='descripcion'><?php echo $director = $_POST['director']; ?></span></br>
+                    <span class='texto'>Guion: </span><span class='descripcion'><?php echo $guion = $_POST['guion']; ?></span></br>
+                    <span class='texto'>Produccion: </span><span class='descripcion'><?php echo $produccion = $_POST['produccion']; ?></span></br>
+                    <span class='texto'>Año: </span><span class='descripcion'><?php echo $anio = $_POST["anio"]; ?></span></br>
+                    <span class='texto'>Nacionalidad: </span><span class='descripcion'><?php echo $nacionalidad = $_POST['nacionalidad']; ?></span></br>
+                    <span class='texto'>Genero: </span><span class='descripcion'><?php echo $genero = $_POST['genero']; ?></span></br>
+                    <span class='texto'>Duracion: </span><span class='descripcion'><?php echo $duracion = $_POST['duracion']; ?></span></br>
+                    <span class='texto'>Restricciones de Edad: </span><span class='descripcion'><?php echo $restriccion = $_POST['edad']; ?></span></br>
+                    <span class='texto'>Sinopsis: </span><span class='descripcion'><?php echo $sinopsis = $_POST['sinopsis']; ?></span></br>
                 </div>  
             </div>
 </div>
