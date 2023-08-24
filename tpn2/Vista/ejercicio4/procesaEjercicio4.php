647c96c767d931c578928c50da3358db7730ed19
PABLO NAVARRO <118768679+pablonq@users.noreply.github.com>
2023-08-24 12:28:16 -0300

    Update procesaEjercicio4.php

--- tpn2/Vista/ejercicio4/procesaEjercicio4.php
+++ tpn2/Vista/ejercicio4/procesaEjercicio4.php
@@ -1,29 +1,10 @@
-<html>
-    <head>
-        <title>ver numero</title>
-    </head>
-    <body>
-        <h1>Procesa</h1>
-        <?php
-            if ($_POST){
-
-                $numero = $_POST['numero'] ;
-
-                if ($numero == ""){
-                    echo "No se ingreso ningún número<br>";
-                } else if($numero > 0){
-                    echo "El número ingresado es positivo<br>";
-                } else if ($numero < 0){
-                    echo "El número ingresado es negativo<br>";
-                } else {
-                    echo "El número ingresado es cero<br>";
-                }
-                echo "El valor del número ingresado es: ".$numero."<br>";
-
-            }else{
-                echo "No se recibieron datos<br>";
-            }
-        ?>
-        <a href="../vista/ejercicio1.php"> Volver <a>
-    </body>
-</html>
+<?php 
+    $tituloPagina = "Ejercicio 1 del TP2";
+    $tp = "botonTP2";
+    $ejercicio = "botonEjer1";
+    
+    include_once('../estructura/encabezado.php');
+?>
+            <!-- 
+                tp2 ejercicio 1
+            -->
