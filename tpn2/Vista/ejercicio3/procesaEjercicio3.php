86780feee95a496f167709c093325a2071098fb9
Alter1412 <florianalaforgia@hotmail.com.ar>
2023-08-24 04:05:46 -0300

    ejercicio 3
    Los mensajes de error hay que hubicarlos en otro lugar que no los tape los placeholder Username y Password

--- tpn2/Vista/ejercicio3/procesaEjercicio3.php
+++ tpn2/Vista/ejercicio3/procesaEjercicio3.php
@@ -1,29 +1,52 @@
-<html>
-    <head>
-        <title>ver numero</title>
-    </head>
-    <body>
-        <h1>Procesa</h1>
-        <?php
-            if ($_POST){
+<?php 
+    $tituloPagina = "Ejercicio 1 del TP2";
+    $tp = "botonTP2";
+    $ejercicio = "botonEjer1";
+    
+    include_once('../estructura/encabezado.php');
+?>
+            <!-- 
+                tp2 ejercicio 1
+            -->
 
-                $numero = $_POST['numero'] ;
-
-                if ($numero == ""){
-                    echo "No se ingreso ningún número<br>";
-                } else if($numero > 0){
-                    echo "El número ingresado es positivo<br>";
-                } else if ($numero < 0){
-                    echo "El número ingresado es negativo<br>";
-                } else {
-                    echo "El número ingresado es cero<br>";
+            <div class="contenedorCentrado">
+                <?php
+                 if($_POST){
+                    $usuario=$_POST["usuario"];
+                    $pass=$_POST["pass"];
+                    $datos=[];
+                    $datos["usuario"]=["Alter","Flor"];
+                    $datos["clave"]=["Alter1412","Flor1234"];
+                    $i=0;
+                    $j=0;
+                    $cuenta=false;
+                    $contra=false;
+            
+                    while(($i<count($datos["usuario"]) && !$cuenta)){
+                        if($usuario==$datos["usuario"][$i]){
+                            $cuenta=true;
+                        }
+                        $i++;
+                    }
+            
+                    while(($j<count($datos["clave"]) && !$contra)){
+                        if($pass==$datos["clave"][$j]){
+                            $contra=true;
+                        }
+                        $j++;
+                    }
+            
+                    if($cuenta && $contra){
+                        echo "Bienvenido!";
+                    }else{
+                        echo "Contraseña uh usuario no validos";
+                    }
+                }else{
+                    echo "Error";
                 }
-                echo "El valor del número ingresado es: ".$numero."<br>";
+                ?>
+            </div>
 
-            }else{
-                echo "No se recibieron datos<br>";
-            }
-        ?>
-        <a href="../vista/ejercicio1.php"> Volver <a>
-    </body>
-</html>
+<?php
+    include_once('../estructura/pie.php');
+?>
--- tpn2/Vista/estructura/encabezado.php
+++ tpn2/Vista/estructura/encabezado.php
@@ -9,6 +9,13 @@
         <link rel="preconnect" href="https://fonts.googleapis.com">
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
         <link href="https://fonts.googleapis.com/css2?family=Michroma&display=swap" rel="stylesheet">
+        <script  src="https://code.jquery.com/jquery-3.7.0.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.19.5/jquery.validate.min.js"></script>
+    <script src="../control/verificacio_jquery.js"></script>
+    <script src="../js/jquery_spanish.js"></script>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
 
     </head>
     <body onload="actualizarCurrent(<?php echo $tp ?>, <?php echo $ejercicio ?>)">
