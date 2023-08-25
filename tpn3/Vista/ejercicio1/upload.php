8cf90437d16d63efe35f133a4346676cc3dc659d
PaulyPAREDES <pauly.pards@gmail.com>
2023-08-24 21:58:13 -0300

    control crecaion objetos tp3 eje 1 y 2

--- tpn3/Vista/ejercicio1/upload.php
+++ tpn3/Vista/ejercicio1/upload.php
@@ -4,49 +4,30 @@
     $ejercicio = "botonEjer1";
     
     include_once('../estructura/encabezado.php');
+    include_once('../../control/controlEjercio1/cargaPdf.php');
 ?>
 
     <div class="contenedorCentrado">
 
 <?php
     // controles de archivo
-    
-    $subirOk = true;
-    
-    
+  
     $nombreArchivo= strtolower( $_FILES['miArchivo']['name'] ); 
-    
-    //Busco en el nombre si aparece pdf o doc
-    
-    $mystring = $nombreArchivo;
-    $encontrar   = '.pdf';
-    $encontrar2   = '.doc';
-    $pos1 = strpos($mystring, $encontrar);
-    $pos2 = strpos($mystring, $encontrar2);
-    
-     //controar formatos
-     if($pos1=== false || $pos1=== false) {
-       echo "Lo siento solo se permiten archivos PDF o DOC. \n";
-       $subirOk = false;
-     }
-     // chuequear tamano
-    if ($_FILES['miArchivo']["size"] > 2000000) {
-        echo "El tamano supera el limite. max 2mb";
-        $subirOk = false;
-      }
-    
-    if ($subirOk == false) {
-    
-      echo "ERROR: no se pudo cargar el archivo";
-    }else{
-      if (move_uploaded_file($_FILES['miArchivo']['tmp_name'],'../../archivos/'.$nombreArchivo)){
-        echo "El archivo ".$nombreArchivo." se ha sido subido con Éxito <br />";
-        // escribir html en php se usa comillas simples
-        echo "<a href='../../archivos/'.$nombreArchivo.>Ver archivo</a>";
-        
-      }else{
-        echo "Lo siento, hubo un error al cargar su archivo.";
-      }
+
+    $objetopdf= new cargaPdf();
+    $mensaje= $objetopdf->analizarArchivo($nombreArchivo);
+
+    if($mensaje == 1){
+     echo "Lo siento solo se permiten archivos PDF o DOC. \n";
+    }else if($mensaje == 2){
+      echo "El tamano supera el limite. max 2mb";
+    }
+    else if($mensaje == 3){
+      echo "El archivo ".$nombreArchivo." se ha sido subido con Éxito <br />";
+      echo '<a href="../../archivos/">Ver archivo</a>';
+    }
+    else{
+      echo "Lo siento, hubo un error al cargar su archivo.";
     }
     ?>
     </div>
--- tpn3/Vista/ejercicio2/upload.php
+++ tpn3/Vista/ejercicio2/upload.php
@@ -4,34 +4,32 @@
     $ejercicio = "botonEjer2";
     
     include_once('../estructura/encabezado.php');
-?>
+    include_once('../../control/controlEjercio2/cargaText.php');
+    ?>
 
     <div class="contenedorCentrado">
-
-    <?php
-$nombreArchivo= strtolower( $_FILES['miArchivo']['name'] ); 
-
-//Busco en el nombre si aparece pdf o doc
-
-$mystring = $nombreArchivo;
-$encontrar   = '.txt';
-$pos = strpos($mystring, $encontrar);
-
- //controlar formatos
- if($pos=== false ) {
-   $mensaje="<h3>Lo siento solo se permiten archivos txt. <h3>\n";
  
- }else if
-   (move_uploaded_file($_FILES['miArchivo']['tmp_name'],'../../archivos/'.$nombreArchivo)){
+    <?php
+ $nombreArchivo= strtolower( $_FILES['miArchivo']['name'] ); 
 
+ $objetotex= new cargaTex();
+ $mensaje= $objetotex->analizarArchivoTex($nombreArchivo);
+
+ if($mensaje == 0){
+  echo "<h3>Lo siento solo se permiten archivos txt. <h3>\n";
+ }
+ else{
     //convertir el contenido en un text area
-   $archivo = file_get_contents('../../archivos/'.$nombreArchivo);
-   $mensaje= "<h3>Este es el contenido de su archivo de texto cargado:<h3> <br/>
-    <textarea  rows='30' cols='70'> $archivo </textarea>";
-  }
-echo $mensaje;
-?>
-</div>
+    $archivo = file_get_contents('../../archivos/'.$nombreArchivo);
+
+            
+    echo "<div class='contenedorEnunciado'>
+    <p>Este es el contenido de su archivo de texto cargado</p>
+    </div>
+     <textarea  rows='30' cols='50'> $archivo </textarea>";
+ }
+ ?>
+  </div>
 <?php
     include_once('../estructura/pie.php');
 ?>
