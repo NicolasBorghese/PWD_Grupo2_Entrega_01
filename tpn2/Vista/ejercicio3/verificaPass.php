58a6e302817d976ac0cf938b4a5dc353377e43fc
GisellaMoreno7 <gisellamoreno2002@gmail.com>
2023-08-25 00:03:18 -0300

    Ajuste

--- tpn2/Vista/ejercicio3/verificaPass.php
+++ tpn2/Vista/ejercicio3/verificaPass.php
@@ -1,52 +1,52 @@
-<?php 
-    $tituloPagina = "Ejercicio 1 del TP2";
-    $tp = "botonTP2";
-    $ejercicio = "botonEjer1";
-    
-    include_once('../estructura/encabezado.php');
+<?php
+$tituloPagina = "Ejercicio 1 del TP2";
+$tp = "botonTP2";
+$ejercicio = "botonEjer1";
+
+include_once('../estructura/encabezado.php');
 ?>
-            <!-- 
+<!-- 
                 tp2 ejercicio 1
             -->
 
-            <div class="contenedorCentrado">
-                <?php
-                 if($_POST){
-                    $usuario=$_POST["usuario"];
-                    $pass=$_POST["pass"];
-                    $datos=[];
-                    $datos["usuario"]=["Alter","Flor"];
-                    $datos["clave"]=["Alter1412","Flor1234"];
-                    $i=0;
-                    $j=0;
-                    $cuenta=false;
-                    $contra=false;
-            
-                    while(($i<count($datos["usuario"]) && !$cuenta)){
-                        if($usuario==$datos["usuario"][$i]){
-                            $cuenta=true;
-                        }
-                        $i++;
-                    }
-            
-                    while(($j<count($datos["clave"]) && !$contra)){
-                        if($pass==$datos["clave"][$j]){
-                            $contra=true;
-                        }
-                        $j++;
-                    }
-            
-                    if($cuenta && $contra){
-                        echo "Bienvenido!";
-                    }else{
-                        echo "Contraseña uh usuario no validos";
-                    }
-                }else{
-                    echo "Error";
-                }
-                ?>
-            </div>
+<div class="contenedorCentrado">
+    <?php
+    if ($_POST) {
+        $usuario = $_POST["usuario"];
+        $pass = $_POST["pass"];
+        $datos = [];
+        $datos["usuario"] = ["Alter", "Flor"];
+        $datos["clave"] = ["Alter1412", "Flor1234"];
+        $i = 0;
+        $j = 0;
+        $cuenta = false;
+        $contra = false;
+
+        while (($i < count($datos["usuario"]) && !$cuenta)) {
+            if ($usuario == $datos["usuario"][$i]) {
+                $cuenta = true;
+            }
+            $i++;
+        }
+
+        while (($j < count($datos["clave"]) && !$contra)) {
+            if ($pass == $datos["clave"][$j]) {
+                $contra = true;
+            }
+            $j++;
+        }
+
+        if ($cuenta && $contra) {
+            echo "Bienvenido!";
+        } else {
+            echo "Contraseña/usuario inválidos.";
+        }
+    } else {
+        echo "Error";
+    }
+    ?>
+</div>
 
 <?php
-    include_once('../estructura/pie.php');
+include_once('../estructura/pie.php');
 ?>
