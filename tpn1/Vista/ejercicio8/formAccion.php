cb2b5499f255d1ed8ff6c77f698890b393d38bde
GisellaMoreno7 <gisellamoreno2002@gmail.com>
2023-09-14 03:34:34 -0300

    Renombramiento + Utiles + configuracion.php (TP1)

---
+++ tpn1/Vista/ejercicio8/formAccion.php
@@ -0,0 +1,40 @@
+<?php
+
+include_once("../../Control/control8/CalculaTarifa.php");
+
+if ($_POST) {
+    $edad = $_POST['edad'];
+    $estudia = $_POST['estudia'];
+
+    $obj = new CalculaTarifa;
+    $tarifa = $obj->calcularTarifa($edad, $estudia);
+}
+
+$tituloPagina = "Ejercicio 8 del TP1";
+$tp = "botonTP1";
+$ejercicio = "botonEjer8";
+
+include_once('../estructura/encabezado.php');
+?>
+
+<div class="contenedorEnunciado">
+    Calcular tarifa para entrada de cine
+</div>
+<div class="contenedorCentrado">
+    <?php
+
+    if ($tarifa != "error") {
+        echo "El precio de la entrada es de: $" . $tarifa;
+    } else {
+        echo "error al ingresar los datos";
+    }
+
+    ?>
+    <div id="contieneLinkVolver">
+        <a href="ejercicio8.php" id="linkVolver"><br> Volver <a>
+    </div>
+</div>
+
+<?php
+include_once('../estructura/pie.php');
+?>
