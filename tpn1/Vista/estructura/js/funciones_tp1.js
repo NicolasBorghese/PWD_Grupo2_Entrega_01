70c1de4e150ffd7da7ef623c7922d350e2806ceb
GisellaMoreno7 <gisellamoreno2002@gmail.com>
2023-09-14 04:07:57 -0300

    Ajuste funciones_tp1.js

--- tpn1/Vista/estructura/js/funciones_tp1.js
+++ tpn1/Vista/estructura/js/funciones_tp1.js
@@ -3,7 +3,7 @@
  * Lee elementos que corresponden al ejercicio en el que se está actualmente y los remarca en el navegador
  * 
  */
-function actualizarCurrent(tp, ejercicio){
+function actualizarCurrent(tp, ejercicio) {
 
     tp.classList.add("botonActual");
     ejercicio.classList.add("botonActual");
@@ -13,48 +13,48 @@ function actualizarCurrent(tp, ejercicio){
 ||                                    Links del navegador                                  ||
 \*\=======================================================================================/*/
 
-function irATP1Ejercicio1(){
-    window.location.href = "../ejercicio1/ejercicio1.php";
+function irATP1Ejercicio1() {
+    window.location.href = "../ejercicio1/formulario.php";
 }
 
-function irATP1Ejercicio2(){
-    window.location.href = "../ejercicio2/ejercicio2.php";
+function irATP1Ejercicio2() {
+    window.location.href = "../ejercicio2/formulario.php";
 }
 
-function irATP1Ejercicio3(){
-    window.location.href = "../ejercicio3/ejercicio3.php";
+function irATP1Ejercicio3() {
+    window.location.href = "../ejercicio3/formulario.php";
 }
 
-function irATP1Ejercicio4(){
-    window.location.href = "../ejercicio4/ejercicio4.php";
+function irATP1Ejercicio4() {
+    window.location.href = "../ejercicio4/formulario.php";
 }
 
-function irATP1Ejercicio5(){
-    window.location.href = "../ejercicio5/ejercicio5.php";
+function irATP1Ejercicio5() {
+    window.location.href = "../ejercicio5/formulario.php";
 }
 
-function irATP1Ejercicio6(){
-    window.location.href = "../ejercicio6/ejercicio6.php";
+function irATP1Ejercicio6() {
+    window.location.href = "../ejercicio6/formulario.php";
 }
 
-function irATP1Ejercicio7(){
-    window.location.href = "../ejercicio7/ejercicio7.php";
+function irATP1Ejercicio7() {
+    window.location.href = "../ejercicio7/formulario.php";
 }
 
-function irATP1Ejercicio8(){
-    window.location.href = "../ejercicio8/ejercicio8.php";
+function irATP1Ejercicio8() {
+    window.location.href = "../ejercicio8/formulario.php";
 }
 
 /*/=======================================================================================/*/
 
-function irATP2Ejercicio1(){
+function irATP2Ejercicio1() {
     window.location.href = "../../../tpn2/Vista/ejercicio1/ejercicio1.php";
 }
 
-function irATP3Ejercicio1(){
+function irATP3Ejercicio1() {
     window.location.href = "../../../tpn3/Vista/ejercicio1/ejercicio1.php";
 }
 
-function irATP4Ejercicio1(){
+function irATP4Ejercicio1() {
     window.location.href = "../../../tpn4/Vista/ejercicio1/ejercicio1.php";
 }
