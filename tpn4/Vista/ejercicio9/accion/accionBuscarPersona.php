38f0581f1d9eaeb21b5d7cd562ada53bd8e8199b
NicolasBorghese <nicolasborghese@hotmail.com>
2023-09-15 15:18:42 -0300

    Corrección Ejercicio 9 tp4

--- tpn4/Vista/ejercicio9/accion/accionBuscarPersona.php
+++ tpn4/Vista/ejercicio9/accion/accionBuscarPersona.php
@@ -13,10 +13,6 @@
 
     $listaPersona=$objPersona->buscar($datos);
     $contListaP = count($listaPersona);
-
-    if ($contListaP == 0){
-        echo '<h4>NO existe persona cargada con ese DNI </h4>?';
-    }
 ?>
     <!-- 
         tp4 ejercicio 9
@@ -24,85 +20,87 @@
 
     <div class="contenedorCentrado">
         <?php
+            if ($contListaP == 0){
+                echo '<h4>NO existe persona cargada con ese DNI </h4>';
+            } else {
+                echo "<strong>DATOS DE LA PERSONA BUSCADA DNI:".$datos['NroDni']."</strong>";
+                echo "<h4>Coloque los DATOS que desea modificar</h4>";
+                echo "<form method='post' action='actualizarDatosPersona.php' class='needs-validation' novalidate>";
+                echo "<table>";
+                echo "<tr>";
+                echo "<td>Dni:</td>";
+                echo "<td>";
+                echo "<input type='text'  id='NroDni' name='NroDni'  value=".$listaPersona[0]->getNroDni()." placeholder='22985265' pattern='[0-9]{8}' maxlength='8' required readonly>";
+                echo " <div class='valid-feedback'>
+                                Muy bien!
+                            </div>
+                            <div class='invalid-feedback'>
+                                Debe ingresar un DNI!
+                            </div>
+                        </td>
+                        </tr>
+                        <tr>
+                            <td>Apellido:</td>";
+                echo "<td><input type='text' value='".$listaPersona[0]->getApellido()."' id='Apellido' name='Apellido' pattern='([a-zA-Z]{1,20})' placeholder='Apellido' required>";
+                echo "<div class='valid-feedback'>
+                            Muy bien!
+                        </div>
+                        <div class='invalid-feedback'>
+                            Debe ingresar un apellido!
+                        </div>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>Nombre:</td>";
+                echo "<td><input type='text' value='".$listaPersona[0]->getNombre()."'  id='Nombre' name='Nombre' pattern='([a-zA-Z]{1,20})' placeholder='Nombre' required>";
+                echo "<div class='valid-feedback'>
+                            Muy bien!
+                        </div>
+                        <div class='invalid-feedback'>
+                            Debe ingresar un nombre!
+                        </div>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>Fecha Nacimiento:</td>";
+                echo "<td><input type='date' value='".$listaPersona[0]->getFechaNac()."' id='fechaNac' name='fechaNac' placeholder='1922-01-01' min='1922-01-01' max='2004-01-01' required>";
+                echo "<div class='valid-feedback'>
+                            Muy bien!
+                        </div>
+                        <div class='invalid-feedback'>
+                        Revise la fecha!
+                        </div>
+                    </td>
+                    </tr>
+                    <tr>
+                        <td>Telefono:</td>";
+                echo "<td><input type='text' value='".$listaPersona[0]->getTelefono()."' id='Telefono' name='Telefono' pattern='\d{3}[-]{1}\d{7}' maxlength='11' placeholder='299-9854155' required>";
+                echo "<div class='valid-feedback'>
+                            Muy bien!
+                        </div>
+                        <div class='invalid-feedback'>
+                        Debe ingresar codigo de area sin 0 '-' y tel sin 15!
+                        </div>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>Domicilio:</td>";
+                echo "<td><input type='text' value='".$listaPersona[0]->getDomicilio()."' id='Domicilio' name='Domicilio' placeholder='Sarmiento 55' required>";
+                echo "<div class='valid-feedback'>
+                                Muy bien!
+                            </div>
+                            <div class='invalid-feedback'>
+                                Debe ingresar un domicilio!
+                            </div>
+                            </td>
+                        </tr>
+                </table>
+                <input type='submit' name='boton_enviar' class='btn btn-dark mt-2' id='boton_enviar' value='MODIFICAR DATOS'>
+                </form>
+                <script src='../estructura/js/validarCamposVacios.js'></script>";
+            }
         ?>
-    <strong>DATOS DE LA PERSONA BUSCADA <?php echo "DNI:".$datos['NroDni'];?></strong>
-      <h4>Coloque los DATOS que desea modificar</h4>
-   <form method="post" action="actualizarDatosPersona.php" class="needs-validation" novalidate>
-   <table>
-            <tr>
-                <td>Dni:</td>
-                <td>
-                <input type="text"  id="NroDni" name="NroDni"  value=<?php echo $listaPersona[0]->getNroDni()?> placeholder="22985265" pattern="[0-9]{8}" maxlength="8" required readonly>
-                <div class="valid-feedback">
-                    Muy bien!
-                </div>
-                <div class="invalid-feedback">
-                    Debe ingresar un DNI!
-                </div>
-               </td>
-            </tr>
-            <tr>
-                <td>Apellido:</td>
-                <td><input type="text" value='<?php echo $listaPersona[0]->getApellido() ?>' id="Apellido" name="Apellido" pattern="([a-zA-Z]{1,20})" placeholder="Apellido" required>
-                <div class="valid-feedback">
-                    Muy bien!
-                </div>
-                <div class="invalid-feedback">
-                    Debe ingresar un apellido!
-                </div>
-                </td>
-            </tr>
-            <tr>
-                <td>Nombre:</td>
-                <td><input type="text" value='<?php echo $listaPersona[0]->getNombre() ?>'  id="Nombre" name="Nombre" pattern="([a-zA-Z]{1,20})" placeholder="Nombre" required>
-                <div class="valid-feedback">
-                    Muy bien!
-                </div>
-                <div class="invalid-feedback">
-                    Debe ingresar un nombre!
-                </div>
-                </td>
-            </tr>
-            <tr>
-                <td>Fecha Nacimiento:</td>
-                <td><input type="date" value='<?php echo $listaPersona[0]->getFechaNac() ?>' id="fechaNac" name="fechaNac" placeholder="1922-01-01" min="1922-01-01" max="2004-01-01" required>
-                <div class="valid-feedback">
-                    Muy bien!
-                </div>
-                <div class="invalid-feedback">
-                   Revise la fecha!
-                </div>
-            </td>
-            </tr>
-            <tr>
-                <td>Telefono:</td>
-                <td><input type="text" value='<?php echo $listaPersona[0]->getTelefono() ?>' id="Telefono" name="Telefono" pattern="\d{3}[-]{1}\d{7}" maxlength="11" placeholder="299-9854155" required>
-                <div class="valid-feedback">
-                    Muy bien!
-                </div>
-                <div class="invalid-feedback">
-                  Debe ingresar codigo de area sin 0 "-" y tel sin 15!
-                </div>
-                </td>
-            </tr>
-            <tr>
-                <td>Domicilio:</td>
-                <td><input type="text" value='<?php echo $listaPersona[0]->getDomicilio() ?>' id="Domicilio" name="Domicilio" placeholder="Sarmiento 55" required>
-                <div class="valid-feedback">
-                    Muy bien!
-                </div>
-                <div class="invalid-feedback">
-                    Debe ingresar un domicilio!
-                </div>
-                </td>
-            </tr>
-       </table>
-       <input type="submit" name="boton_enviar" class="btn btn-dark mt-2" id="boton_enviar" value="MODIFICAR DATOS">
-    </form>
-    <script src="../estructura/js/validarCamposVacios.js"></script>
-
     </div>
-
 <?php
     include_once('../../estructura/pie.php');
 ?>
